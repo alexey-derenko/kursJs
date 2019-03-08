02f77903824a7ee18b518ef875de811c784e39a9
Alexey <alexey.derenko@gmail.com>
2019-03-08 22:53:26 +0200

    Домашка 5-ый урок

---
+++ lesson05/task2/src/js/main.js
@@ -0,0 +1,28 @@
+
+let btnStart = document.querySelector('#start'),
+    allValue = document.querySelectorAll('.budget-value, .level-value, .expenses-value, .optionalexpenses-value, .income-value, .monthsavings-value, .yearsavings-value'),
+    allInput = document.querySelectorAll('.expenses-item'),
+    btnEib = document.getElementsByTagName("button"),
+    btnCbb = document.getElementsByTagName("button"),
+    allInp = document.querySelectorAll('.optionalexpenses-item'),
+    article = document.querySelector('#income'),
+    check = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value'); 
+
+console.log(btnStart);
+console.log(allValue); 
+console.log(allInput);
+console.log(btnEib[0]);
+console.log(btnCbb[2]);
+console.log(allInp);
+console.log(article);
+console.log(check);
+console.log(sum);
+console.log(percent);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
