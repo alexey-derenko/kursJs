31beeb43bd8b60bd9930149d58b3c7348bc1b2fa
Alexey <alexey.derenko@gmail.com>
2019-03-16 15:51:33 +0200

    ES 6 example

---
+++ lesson10/js/script.js
@@ -0,0 +1,152 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    
+    // Timer
+
+    let deadline = '2018-03-15';
+
+    function checkTime(i) {
+        if ( i < 10) {
+            i="0" + i;
+        }
+    return i;
+    }
+
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),        
+        seconds = Math.floor((t/1000)%60),
+        minutes = Math.floor((t/1000/60)%60),
+        hours = Math.floor((t/(1000*60*60)));
+
+      
+
+        if (seconds < 10) {
+            seconds = '0'+ seconds;
+        }
+
+        if (minutes < 10) {
+            minutes = '0'+ minutes;
+        }
+
+        if (hours < 10 ) {
+            hours = '0'+ hours;
+        }
+
+        if (Date.parse(endtime) <= Date.parse(new Date())) {
+            seconds = '00';
+            minutes = '00';
+            hours = '00';
+
+        }
+        
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);                
+            }           
+
+        }    
+    }
+
+    setClock('timer', deadline);
+   
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        let showModal = () => {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden'; 
+            //запрещаем прокрутку фона при открытом модальном окне
+        };
+        showModal();
+        
+    });
+
+    close.addEventListener('click', function() {
+        let closeModal = () => {
+            overlay.style.display = 'none';
+            more.classList.remove('more-splash');
+            document.body.style.overflow = '';
+        };
+        closeModal();
+    });
+    
+    let descriptionBtn = document.querySelectorAll('.description-btn'); 
+    //используем селекторолл так как в проекте много классов .description-btn 
+    //и работаем с массивом кнопок
+
+    descriptionBtn.forEach(function (item) {
+        //перебираем массив конопок .description-btn
+        item.addEventListener('click', function() {
+            let first = () => { //пример стрелочной функции, можно и без нее
+                overlay.style.display = 'block';
+                this.classList.add('more-splash');
+                document.body.style.overflow = 'hidden';
+            };
+            first();        
+        });
+    });     
+});
+
+
+//
